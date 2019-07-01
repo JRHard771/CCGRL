5145bd0ce552eddadc3ab865c5bfc12aa4329962
Joshua Hard <8053162+JRHard771@users.noreply.github.com>
2019-07-01 06:22:53 -0500

    Change fonts; add 3 costs to cards

--- Global/GameData.gd
+++ Global/GameData.gd
@@ -4,12 +4,16 @@ const card = {
 	"Godot": {
 		"image": "res://UI/icon.png",
 		"desc": "[fill]Play this [b]card[/b] to win the [b][color=#88ddff]game[/color][/b]!\nJust look at all the [u]exciting[/u] formatting options afforded by the [i]RichTextLabel[/i] Node!",
-		"cost": 12
+		"cost_str": 12,
+		"cost_agi": 0,
+		"cost_int": 0
 	},
 	"Test": {
-		"image": "res://baddie.png",
-		"desc": "This [b][i]TEST[/i][/b] card be merely a test, [i]yaaaaaarrrrrgh![/i]",
-		"cost": 42
+		"image": "res://Art/GUI/GUI0.png",
+		"desc": "Testing, testing; one, two, three...",
+		"cost_str": 3,
+		"cost_agi": 0,
+		"cost_int": 0
 	}
 }
 
--- UI/Card.gd
+++ UI/Card.gd
@@ -2,6 +2,10 @@ extends Control
 
 var hovered = false
 var saved_rotation = 0
+var cost_str = 0 setget set_cost_str, get_cost_str
+var cost_agi = 0 setget set_cost_agi, get_cost_agi
+var cost_int = 0 setget set_cost_int, get_cost_int
+
 onready var dest_scale = self.rect_scale
 onready var dest_position = self.rect_position
 onready var dest_rotation = self.rect_rotation
@@ -39,4 +43,50 @@ func set_card(new_id):
 	$VBoxContainer/Title.bbcode_text = "[center]%s[/center]" % new_id
 	$VBoxContainer/TextureRect.texture = load(GameData.card[new_id]["image"])
 	$VBoxContainer/Description.bbcode_text = GameData.card[new_id]["desc"]
-	$VBoxContainer/CostText.set_cost(GameData.card[new_id]["cost"])
+	set_cost_str(GameData.card[new_id]["cost_str"])
+	set_cost_agi(GameData.card[new_id]["cost_agi"])
+	set_cost_int(GameData.card[new_id]["cost_int"])
+
+func update_cost():
+	if not cost_str:
+		$VBoxContainer/CostContainer/HBoxContainer/StrengthRect.hide()
+		$VBoxContainer/CostContainer/HBoxContainer/StrCostLabel.hide()
+	else:
+		$VBoxContainer/CostContainer/HBoxContainer/StrengthRect.show()
+		$VBoxContainer/CostContainer/HBoxContainer/StrCostLabel.show()
+		$VBoxContainer/CostContainer/HBoxContainer/StrCostLabel.text = str(cost_str)
+	if not cost_agi:
+		$VBoxContainer/CostContainer/HBoxContainer/AgilityRect.hide()
+		$VBoxContainer/CostContainer/HBoxContainer/AgiCostLabel.hide()
+	else:
+		$VBoxContainer/CostContainer/HBoxContainer/AgilityRect.show()
+		$VBoxContainer/CostContainer/HBoxContainer/AgiCostLabel.show()
+		$VBoxContainer/CostContainer/HBoxContainer/AgiCostLabel.text = str(cost_agi)
+	if not cost_int:
+		$VBoxContainer/CostContainer/HBoxContainer/IntelligenceRect.hide()
+		$VBoxContainer/CostContainer/HBoxContainer/IntCostLabel.hide()
+	else:
+		$VBoxContainer/CostContainer/HBoxContainer/IntelligenceRect.show()
+		$VBoxContainer/CostContainer/HBoxContainer/IntCostLabel.show()
+		$VBoxContainer/CostContainer/HBoxContainer/IntCostLabel.text = str(cost_int)
+
+func set_cost_str(val):
+	cost_str = val
+	update_cost()
+
+func get_cost_str():
+	return cost_str
+
+func set_cost_agi(val):
+	cost_agi = val
+	update_cost()
+
+func get_cost_agi():
+	return cost_agi
+
+func set_cost_int(val):
+	cost_int = val
+	update_cost()
+
+func get_cost_int():
+	return cost_int
